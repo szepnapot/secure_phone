0a76af63f4e42bf590c22de0e56782b2fae3bf79
Peter Lodri <16125384+szepnapot@users.noreply.github.com>
2019-04-08 16:01:32 +0200

    init

---
+++ compile_kernel.sh
@@ -0,0 +1,5 @@
+git clone --recurse-submodules -j8 https://github.com/kerneltoast/android_kernel_google_bluecross.git
+cd android_kernel_google_bluecross
+make
+rm -fr ~/rattlesnake-os/device/google/crosshatch-kernel/*
+cp -R out/arch/arm64/boot/* ~/rattlesnake-os/device/google/crosshatch-kernel/
